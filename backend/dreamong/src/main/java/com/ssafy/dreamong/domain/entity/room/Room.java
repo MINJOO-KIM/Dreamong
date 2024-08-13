c69b6ee6998d8a2be3bbc3dad716aab4a181c116
남혁준 <gurwns0000@gmail.com>
2024-08-14 00:45:56 +0900

    refactor: 네이밍 컨벤션에 맞추도록 변경

--- backend/dreamong/src/main/java/com/ssafy/dreamong/domain/entity/room/Room.java
+++ backend/dreamong/src/main/java/com/ssafy/dreamong/domain/entity/room/Room.java
@@ -11,23 +11,26 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "room")
 public class Room extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "room_id")
     private Integer id;
+
     @Column(name = "title", nullable = false)
     private String title;
+
     @Column(name = "youtube_link", nullable = false, length = 1000)
     private String youtubeLink;
+
     @Column(name = "thumbnail", nullable = false)
     private String thumbnail;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
 
     @Builder
     public Room(Integer id, String title, String youtubeLink, String thumbnail, User user) {
